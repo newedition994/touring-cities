cca9fd17b2fc4d660c731c7a6b7b6459eab27608
Breyanna Vass <breyannavass@gmail.com>
2019-08-20 16:07:56 -0400

    Props displayed

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -4,24 +4,25 @@ import './Tour.scss'
 
 export default class Tour extends Component {
     render() {
+        const { city, img, name, info } = this.props.tour;
         return (
             <article className="tour">
                 <div className="img-container">
-                    <img src="https://images.pexels.com/photos/374870/pexels-photo-374870.jpeg?cs=srgb&dl=architecture-buildings-canada-374870.jpg&fm=jpg" alt="" />
+                    <img src={img} alt="" />
                     <span className="close-btn">
                         <i className="fas fa-window-close" />
                     </span>
                 </div>
                 <div className="tour-info">
-                    <h3>city</h3>
-                    <h4>name</h4>
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
                     <h5>
                         info{" "}
                         <span>
                             <i className="fas fa-caret-square-down" />
                         </span>
                     </h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!</p>
+                    <p>{info}</p>
                 </div>
             </article>
         )
