03b365507d38628a4909d9c6b3495ee95b98636b
Breyanna Vass <breyannavass@gmail.com>
2019-08-20 14:48:44 -0400

    Fixing up some navbar elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Navbar from './components/Navbar/Navbar'
+
 import './App.scss';
 
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <div>
+      <Navbar />
+      <h1>Hello from App</h1>
+    </div>
   );
 }
 
