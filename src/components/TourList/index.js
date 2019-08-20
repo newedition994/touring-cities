6d6c9a3b14554d66043faef1c4b426d7e1e55535
Breyanna Vass <breyannavass@gmail.com>
2019-08-20 15:39:03 -0400

    stylizing the individual tours

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -5,7 +5,7 @@ import Tour from '../Tour/Tour'
 export default class TourList extends Component {
     render() {
         return (
-            <section>
+            <section className="tourlist">
                 <Tour />
             </section>
         )
